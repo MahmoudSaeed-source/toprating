855073c5f6cfb5e9d3fb9cf087fc2cdef8468ece
MAHMOUDSAEED <MahmoudSaid.wd@gmail.com>
2024-12-23 11:36:35 +0200

    navabar

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -35,7 +35,10 @@ const Header = () => {
       <div className="center-nav w-full h-auto flex justify-center items-center m-0 py-4 border-2">
         <div className="container flex justify-between items-center">
           <div className="menu-icons w-12 h-full lg:hidden flex  justify-center items-center text-3xl  mr-2">
-            <FiMenu className="cursor-pointer" onClick={()=>setIsOpen(!isOpen)} />
+            <FiMenu
+              className="cursor-pointer"
+              onClick={() => setIsOpen(!isOpen)}
+            />
           </div>
           <h1 className="logo flex-1 h-full lg:text-5xl  text-3xl text-bold font-play-fair flex justify-center items-center  lg:justify-start">
             TopRating
@@ -55,17 +58,29 @@ const Header = () => {
       </div>
       <nav className="w-full h-10 flex justify-center items-center m-0 p-0  ">
         <div className="container w-full h-full  flex ">
-          <ul className=
-            {`${isOpen ? "w-full h-screen bg-white text-black flex flex-col border-2 border-green-600 md:border-yellow-600 md:w-72": "hidden lg:flex Links w-full  flex-1 h-auto p-0 m-0 list-none  font-bold font-mono text-[15px] uppercas tracking-widest"}`}
+          <ul
+            className={`${
+              isOpen
+                ? "w-full h-auto bg-white text-black flex flex-col md:w-72 border-2 absolute top-34 left-0 "
+                : "hidden lg:flex Links w-full  flex-1 h-auto p-0 m-0 list-none  font-bold font-mono text-[15px] uppercas tracking-widest"
+            }`}
           >
-            <h2 className=
-              {
-              `${isOpen ? " w-full h-12 flex justify-center items-center mt-3 mb-4 uppercase text-3xl text-black border-2 border-red-600":"hidden"}`}>Menu</h2>
-            <li className=
-            {
-              `${isOpen ? "w-full hover:bg-gray-500 transition-all justify-start":"hover:none"} w-32 flex justify-center items-center text-gray-700 py-1`
-            }
-           >
+            <h2
+              className={`${
+                isOpen
+                  ? " w-full h-12 flex justify-center items-center mt-3 mb-4 uppercase text-3xl text-black "
+                  : "hidden"
+              }`}
+            >
+              Menu
+            </h2>
+            <li
+              className={`${
+                isOpen
+                  ? "w-full hover:bg-gray-500 transition-all justify-start border-b-2 py-4"
+                  : "hover:none"
+              } w-auto  flex justify-center items-center text-gray-700 py-1 px-2`}
+            >
               <Link
                 className="w-full h-full flex justify-center items-center uppercase"
                 href="/"
@@ -73,7 +88,13 @@ const Header = () => {
                 Home
               </Link>
             </li>
-            <li className=>
+            <li
+              className={`${
+                isOpen
+                  ? "w-full hover:bg-gray-500 transition-all justify-start border-b-2 py-4 "
+                  : "hover:none"
+              }w-auto  flex justify-center items-center text-gray-700 py-1 px-2`}
+            >
               <Link
                 className="w-full h-full flex justify-center items-center uppercase"
                 href="/shop"
@@ -81,7 +102,13 @@ const Header = () => {
                 Shop
               </Link>
             </li>
-            <li className="w-32 flex justify-center items-center text-gray-700 py-1  ">
+            <li
+              className={`${
+                isOpen
+                  ? "w-full hover:bg-gray-500 transition-all justify-start border-b-2 py-4 "
+                  : "hover:none"
+              }w-auto  flex justify-center items-center text-gray-700 py-1 px-2`}
+            >
               <Link
                 className="w-full h-full flex justify-center items-center uppercase"
                 href="/about"
@@ -89,7 +116,13 @@ const Header = () => {
                 AboutUs
               </Link>
             </li>
-            <li className="w-32 flex justify-center items-center text-gray-700 py-1 ">
+            <li
+              className={`${
+                isOpen
+                  ? "w-full hover:bg-gray-500 transition-all justify-start border-b-2 py-4"
+                  : "hover:none"
+              } w-auto  flex justify-center items-center text-gray-700 py-1 px-2`}
+            >
               <Link
                 className="w-full h-full  flex justify-center items-center uppercase"
                 href="/contact"
@@ -97,7 +130,13 @@ const Header = () => {
                 contact us
               </Link>
             </li>
-            <li className="w-32 flex justify-center items-center text-gray-700 py-1  ">
+            <li
+              className={`${
+                isOpen
+                  ? "w-full hover:bg-gray-500 transition-all justify-start border-b-2 py-4"
+                  : "hover:none"
+              } w-auto flex justify-center items-center text-gray-700 py-4 px-2`}
+            >
               <Link
                 className="w-full h-full  flex justify-center items-center uppercase"
                 href="/massage-us"
@@ -106,16 +145,16 @@ const Header = () => {
               </Link>
             </li>
           </ul>
-          <div className="search_icons-cart lg:flex justify-end items-center hidden">
+          <div className="search_icons-cart lg:flex justify-end items-center hidden font-mono">
             <div className="search  flex justify-end items-center">
               <span className="mr-3 text-lg uppercase">search</span>
               <span className="text-xl mr-4">
-                <CiSearch  />
+                <CiSearch />
               </span>
               <span className="text-xl ">|</span>
             </div>
             <div className="car-wish-list-icons flex justify-start items-center flex-1 text-2xl  ">
-              <IoIosHeartEmpty className="mr-6 ml-4"/>
+              <IoIosHeartEmpty className="mr-6 ml-4" />
               <MdOutlineShoppingBag />
             </div>
           </div>
